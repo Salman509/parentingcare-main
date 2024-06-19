57b20597f0680ec715d930cc5dcd75388354fb27
Lord Cammang <andisalman63@gmail.com>
2024-06-19 16:08:46 +0800

    Update Webpack configuration and project settings

--- webpack.common.js
+++ webpack.common.js
@@ -10,7 +10,7 @@ module.exports = {
   },
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'dist'),  // Pastikan ini sesuai dengan pengaturan Vercel
     clean: true,
   },
   module: {
